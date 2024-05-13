66cd1633e1268c1356ead56be3556cc449d574f0
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2024-05-13 10:40:11 +0200

    nevim jsem total v hajzlu

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.WebSockets;
+using WebSocketSharp.Server;
 
 namespace IT3B_Chat.Server
 {
@@ -21,9 +24,63 @@ namespace IT3B_Chat.Server
    InitializeComponent();
   }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private WebSocketServer server;
+        private List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FleckLog.Level = LogLevel.Debug;
+        }
+        private void BtnStartServer_Click(object sender, RoutedEventArgs e)
+        {
+            StartServer();
+        }
+
+        private void BtnStopServer_Click(object sender, RoutedEventArgs e)
+        {
+            StopServer();
+        }
+
+        private void StartServer()
+        {
+            server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Dispatcher.Invoke(() => listBoxLog.Items.Add("Client Connected!"));
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Dispatcher.Invoke(() => listBoxLog.Items.Add("Client Disconnected!"));
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Dispatcher.Invoke(() => listBoxLog.Items.Add("Received: " + message));
+                    foreach (var s in allSockets)
+                    {
+                        s.Send("Echo: " + message);
+                    }
+                };
+            });
+
+            listBoxLog.Items.Add("Server started at ws://0.0.0.0:8181");
+        }
+
+
+        private void StopServer()
+        {
+            foreach (var socket in allSockets.ToList())
+            {
+                socket.Close();
+            }
+            server.Dispose();
+            listBoxLog.Items.Add("Server stopped.");
         }
     }
+}
 }
